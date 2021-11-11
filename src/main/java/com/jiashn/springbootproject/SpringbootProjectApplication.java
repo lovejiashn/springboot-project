9c94ba146f187ac8d64e3deef3b4f48a0999bbd4
jiangjs <421582841@qq.com>
2021-11-11 10:06:36 +0800

    [创建]创建项目

---
+++ src/main/java/com/jiashn/springbootproject/SpringbootProjectApplication.java
@@ -0,0 +1,16 @@
+package com.jiashn.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangjs
+ */
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProjectApplication.class, args);
+    }
+
+}
