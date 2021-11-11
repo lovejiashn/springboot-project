ec82d4263ac8639bdde000de02c9b137bc1921a2
jiangjs <421582841@qq.com>
2021-11-11 16:13:03 +0800

    [新增]添加数据库等

--- src/main/java/com/jiashn/springbootproject/SpringbootProjectApplication.java
+++ src/main/java/com/jiashn/springbootproject/SpringbootProjectApplication.java
@@ -1,5 +1,6 @@
 package com.jiashn.springbootproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author jiangjs
  */
 @SpringBootApplication
+@MapperScan("com.jiashn.springbootproject.*.entity.*")
 public class SpringbootProjectApplication {
 
     public static void main(String[] args) {
